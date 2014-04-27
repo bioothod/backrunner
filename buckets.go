012b5becc0ed2d633a2bff6039a20d51356390dd
Evgeniy Polyakov <zbr@ioremap.net>
2014-04-28 03:21:44 +0400

    buckets: added buckets delete metadata and common buckets constants

---
+++ buckets.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/vmihailenco/msgpack"
+)
+
+var (
+	DeleteIndex string = "delete"
+	BucketNamespace string = "bucket"
+	Buckets []string = []string{"bucket:11.21", "bucket:22.31", "bucket:32.12"}
+)
+
+type Delentry struct {
+	time int64
+	key string
+}
+
+func (del *Delentry) pack() ([]byte, error) {
+	in := map[string]interface{}{
+		"time": del.time,
+		"key": del.key,
+	}
+	return msgpack.Marshal(in)
+}
+
+func (del *Delentry) unpack(data []byte) (err error) {
+	var out map[string]interface{}
+	err = msgpack.Unmarshal(data, &out)
+	if err != nil {
+		return err
+	}
+
+	del.time = out["time"].(int64)
+	del.key = out["key"].(string)
+
+	return nil
+}
