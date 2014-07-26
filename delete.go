7720b08e927c8626b80a6e71c1ee69300990d600
Evgeniy Polyakov <zbr@ioremap.net>
2014-07-27 03:53:36 +0400

    delete: get rid of postponed deletion logic through secondary indexes

--- delete.go
+++ delete.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/base64"
 	"flag"
 	"fmt"
 	"github.com/bioothod/elliptics-go/elliptics"
@@ -47,12 +46,6 @@ func main() {
 		log.Fatal("Failed to create metadata session", err)
 	}
 
-	data_session, err := elliptics.NewSession(node)
-	if err != nil {
-		log.Fatal("Failed to create data session", err)
-	}
-	data_session.SetTimeout(10)
-
 	var metadata_groups []int32
 	for _, group := range strings.Split(*group_str, ":") {
 		g, err := strconv.Atoi(group)
@@ -83,37 +76,7 @@ func main() {
 				log.Fatal("Unsupported msgpack data:", err)
 			}
 
-			fmt.Printf("bucket: %s: groups: %v\n", meta.bucket, meta.groups)
-
-			data_session.SetGroups(meta.groups)
-			data_session.SetNamespace(meta.bucket)
-		}
-
-		fmt.Printf("bucket: %s: getting indexes\n", meta.bucket)
-		for res := range data_session.FindAnyIndexes([]string{DeleteIndex}) {
-			for _, entry := range res.Data() {
-				data, err := base64.StdEncoding.DecodeString(entry.Data)
-				if err != nil {
-					log.Printf("bucket: %s: could not convert base64 entry to byte array: %v, error: %v\n", meta.bucket, entry.Data, err)
-					continue
-				}
-
-				var del Delentry
-				err = del.unpack(data)
-				if err != nil {
-					log.Printf("bucket: %s: skipping invalid delete entry: %v, error: %v\n", meta.bucket, data, err)
-					continue
-				}
-
-				delstr := "skipping"
-				if time.Now().Unix() > del.time {
-					_ = data_session.RemoveIndexes(del.key, []string{DeleteIndex})
-					_ = data_session.Remove(del.key)
-					delstr = "deleting"
-				}
-
-				fmt.Printf("bucket: %s: %s key: %s, time: %d\n", meta.bucket, delstr, del.key, del.time)
-			}
+			fmt.Printf("bucket: %s: groups: %v, flags: %v, acl: %v\n", meta.bucket, meta.groups, meta.flags, meta.acl)
 		}
 	}
 }
