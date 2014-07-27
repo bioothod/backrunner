d127fec26f8b91b7682d34925a1e35808c1fb7c7
Evgeniy Polyakov <zbr@ioremap.net>
2014-07-27 04:15:16 +0400

    meta: get rid of unused deletion index

--- buckets.go
+++ buckets.go
@@ -3,14 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/vmihailenco/msgpack"
 	"io/ioutil"
 	"os"
 	"strings"
 )
 
 var (
-	DeleteIndex     string   = "delete"
 	BucketNamespace string   = "bucket"
 	Buckets         []string
 )
@@ -28,32 +26,6 @@ func BucketsInit(path string) (err error) {
 			fmt.Printf("bucket: %s\n", str)
 		}
 	}
-
-	return nil
-}
-
-type Delentry struct {
-	time int64
-	key  string
-}
-
-func (del *Delentry) pack() ([]byte, error) {
-	in := map[string]interface{}{
-		"time": del.time,
-		"key":  del.key,
-	}
-	return msgpack.Marshal(in)
-}
-
-func (del *Delentry) unpack(data []byte) (err error) {
-	var out map[string]interface{}
-	err = msgpack.Unmarshal(data, &out)
-	if err != nil {
-		return err
-	}
-
-	del.time = out["time"].(int64)
-	del.key = out["key"].(string)
 
 	return nil
 }
