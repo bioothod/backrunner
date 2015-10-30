90b1735c6e5c2cd7ffb6d264130f0fd8f26dc85c
Evgeniy Polyakov <zbr@ioremap.net>
2015-10-30 18:11:40 +0300

    estimator: fixed rps/bps to actually be accumulating counters, not plain averages

--- estimator/estimator.go
+++ estimator/estimator.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-const ftime float64 = 5.0 // 5 second avg
+const EstimatorRange int = 3
 
 func MovingExpAvg(value, oldValue, fdtime, ftime float64) float64 {
 	alpha := 1.0 - math.Exp(-fdtime/ftime)
@@ -15,17 +15,36 @@ func MovingExpAvg(value, oldValue, fdtime, ftime float64) float64 {
 	return r
 }
 
+type Second struct {
+	Second		int
+	BPS		uint64
+	RPS		uint64
+}
+
 type RequestStat struct {
-	UpdateTime	time.Time		`json:"-"`
+	SStat		[]Second		`json:"-"`
 	BPS		float64
 	RPS		float64
 }
 
 func NewRequestStat() *RequestStat {
 	return &RequestStat {
-		UpdateTime:	time.Now(),
 		BPS:		0,
 		RPS:		0,
+		SStat:		make([]Second, EstimatorRange),
+	}
+}
+
+func (r *RequestStat) Copy(start int) {
+	for i := 0; i < EstimatorRange; i++ {
+		idx := start - i - 1
+		if idx < 0 {
+			idx = EstimatorRange - i - 1
+		}
+
+		s := r.SStat[idx]
+		r.RPS = MovingExpAvg(float64(s.RPS), r.RPS, float64(EstimatorRange - i), float64(EstimatorRange))
+		r.BPS = MovingExpAvg(float64(s.BPS), r.BPS, float64(EstimatorRange - i), float64(EstimatorRange))
 	}
 }
 
@@ -41,7 +60,7 @@ func NewEstimator() *Estimator {
 	}
 }
 
-func (e *Estimator) PushNolock(size uint64, status int) {
+func (e *Estimator) PushNolock(requests, size uint64, status int) {
 	switch {
 	case status >= 200 && status < 300:
 		status = 200
@@ -53,27 +72,32 @@ func (e *Estimator) PushNolock(size uint64, status int) {
 		status = 500
 	}
 
-	fdelta := 1.0 // time passed since last update
-
 	rs, ok := e.RS[status]
 	if !ok {
 		rs = NewRequestStat()
 		e.RS[status] = rs
-	} else {
-		fdelta = time.Now().Sub(rs.UpdateTime).Seconds()
 	}
 
-	rs.RPS = MovingExpAvg(1.0, rs.RPS, fdelta, ftime)
-	rs.BPS = MovingExpAvg(float64(size), rs.BPS, fdelta, ftime)
+	second := time.Now().Second()
 
-	rs.UpdateTime = time.Now()
+	idx := second % EstimatorRange
+	ss := &rs.SStat[idx]
+
+	if ss.Second != second {
+		ss.Second = second
+		ss.RPS = 0
+		ss.BPS = 0
+	}
+
+	ss.RPS += requests
+	ss.BPS += size
 }
 
 func (e *Estimator) Push(size uint64, status int) {
 	e.Lock()
 	defer e.Unlock()
 
-	e.PushNolock(size, status)
+	e.PushNolock(1, size, status)
 }
 
 
@@ -81,14 +105,14 @@ func (e *Estimator) Copy() (map[string]RequestStat) {
 	e.Lock()
 	defer e.Unlock()
 
+	second := time.Now().Second()
+	idx := second % EstimatorRange
+
 	res := make(map[string]RequestStat)
 	for k, v := range e.RS {
-		fdelta := time.Now().Sub(v.UpdateTime).Seconds()
-		if fdelta > ftime {
-			v.RPS = MovingExpAvg(0, v.RPS, fdelta, ftime)
-			v.BPS = MovingExpAvg(0, v.BPS, fdelta, ftime)
-			v.UpdateTime = time.Now()
-		}
+		e.PushNolock(0, 0, k)
+
+		v.Copy(idx)
 
 		if v.RPS > 0.1 {
 			res[strconv.Itoa(k)] = *v
