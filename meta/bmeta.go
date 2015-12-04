106c4a3c9698cd60e1b19d20920c184b0664027e
Evgeniy Polyakov <zbr@ioremap.net>
2015-12-04 21:41:41 +0300

    bmeta: added options to upload bucket list and backrunner config into elliptics

--- meta/bmeta.go
+++ meta/bmeta.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"flag"
+	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/bioothod/backrunner/bucket"
 	"github.com/bioothod/backrunner/config"
@@ -10,6 +11,41 @@ import (
 	"io/ioutil"
 	"log"
 )
+
+func bmeta_read_upload_file(ell *etransport.Elliptics, file, key string) (err error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalf("Could not read file %s: %v", file, err)
+	}
+
+	ms, err := ell.MetadataSession()
+	if err != nil {
+		return
+	}
+	defer ms.Delete()
+
+	ms.SetNamespace(bucket.BucketNamespace)
+
+	tried := false
+
+	for wr := range ms.WriteData(key, bytes.NewReader(data), 0, 0) {
+		tried = true
+
+		if wr.Error() != nil {
+			err = wr.Error()
+
+			log.Printf("%s: could not write file %s: %v", key, file, err)
+			continue
+		}
+
+		return
+	}
+
+	if !tried {
+		err = fmt.Errorf("%s: could not write file %s: WriteData() returned nothing", key, file)
+	}
+	return
+}
 
 func bmeta_read_upload(ell *etransport.Elliptics, file string) (err error) {
 	metaf, err := ioutil.ReadFile(file)
@@ -60,24 +96,32 @@ func bmeta_read_upload(ell *etransport.Elliptics, file string) (err error) {
 func main() {
 	bname := flag.String("bucket", "", "bucket name to read")
 	config_file := flag.String("config", "", "transport config file")
+	backrunner_config := flag.Bool("upload-backrunner-config", false,
+		"forces to upload provided config file into elliptics using 'backrunner-config-key' config parameter")
+	bucket_list_upload := flag.String("upload-bucket-list", "",
+		"bucket list to upload into elliptics using 'bucket-list-key' config parameter")
 	upload := flag.String("upload", "", "bucket json file to upload/rewrite")
 	flag.Parse()
 
-	if *bname == "" && *upload == "" {
-		log.Fatal("You must specify either bucket name to read of upload file with buckets")
+	if *bname == "" && *upload == "" && *backrunner_config == false && *bucket_list_upload == "" {
+		log.Fatal("You must specify one (or more) of the following options:\n" +
+				"* bucket name to read\n" +
+				"* file with buckets metadata to upload\n" +
+				"* list of buckets to upload\n" +
+				"* backrunner config to upload\n")
 	}
 
 	if *config_file == "" {
 		log.Fatal("You must specify Elliptics config file")
 	}
 
-	cnf := &config.ProxyConfig{}
-	err := cnf.Load(*config_file)
+	conf := &config.ProxyConfig{}
+	err := conf.Load(*config_file)
 	if err != nil {
 		log.Fatalf("Could not load config file '%s': %v", *config_file, err)
 	}
 
-	ell, err := etransport.NewEllipticsTransport(cnf)
+	ell, err := etransport.NewEllipticsTransport(conf)
 	if err != nil {
 		log.Fatalf("Could not create Elliptics transport: %v", err)
 	}
@@ -89,12 +133,40 @@ func main() {
 		if err != nil {
 			log.Fatalf("Could not write some buckets: %v", err)
 		}
-	} else {
+	}
+
+	if *bname != "" {
 		b, err = bucket.ReadBucket(ell, *bname)
 		if err != nil {
 			log.Fatalf("Could not read bucket %s: %v", *bname, err)
 		}
 
 		log.Printf("%s\n", b.Meta.String())
+	}
+
+	if *backrunner_config == true {
+		if len(conf.Elliptics.BackrunnerConfig) == 0 {
+			log.Fatalf("Requested uploading %s into elliptics as backrunner config, " +
+				"but there is no 'backrunner-config-key' option",
+				*config_file)
+		}
+
+		err = bmeta_read_upload_file(ell, *config_file, conf.Elliptics.BackrunnerConfig)
+		if err != nil {
+			log.Fatalf("upload: %v", err)
+		}
+	}
+
+	if *bucket_list_upload != "" {
+		if len(conf.Elliptics.BucketList) == 0 {
+			log.Fatalf("Requested uploading %s into elliptics as list of buckets, " +
+				"but there is no 'bucket-list-key' option",
+				*bucket_list_upload)
+		}
+
+		err = bmeta_read_upload_file(ell, *bucket_list_upload, conf.Elliptics.BucketList)
+		if err != nil {
+			log.Fatalf("upload: %v", err)
+		}
 	}
 }
